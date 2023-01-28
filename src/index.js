42eccb9ec01879e0c239fe67822ce3bd2c89c0ba
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-01-28 12:09:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ const formsub=document.querySelector('form');
+  formsub.addEventListener('submit',(e)=>{
+e.preventDefault();
+    const text = document.getElementById('new-task-description')
+    console.log(text.value)
+    let li=document.createElement('li');
+    li.innerHTML = `${text.value}`;
+    const ul = document.getElementById('tasks')
+    ul.appendChild(li)
+    const btn =document.createElement('button')
+    btn.addEventListener('click',handleDelete)
+    btn.textContent='x'
+    li.appendChild(btn)
+  })
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
 });
+
